aa8c4bbcf66b9c5fc99d401e09c7ea9faf491ace
Fuse <flux@whsrp.eu>
2026-02-15 22:15:15 +0100

    1.0

---
+++ fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+author 'Flux'
+github 'https://github.com/fusefivem'
+description 'FD Cocaine | Fuse Development'
+
+dependency 'ox_lib'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua'
+}
+
+files {
+    'locales/*.json'
+}
+
+client_scripts {
+    'game/src/cl/*.lua'
+}
+
+server_scripts {
+    'game/src/sv/*.lua'
+}
