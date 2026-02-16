6745267f90fabe1d02cfe81269cb4ca684e51a91
Fuse <flux@whsrp.eu>
2026-02-16 14:13:08 +0100

    1.0

---
+++ Installation/items.lua
@@ -0,0 +1,51 @@
+	['coke_paste'] = {
+		label = 'Cocaine Paste',
+		weight = 200,
+		stack = true,
+		close = true,
+		description = 'A crude base paste extracted from coca leaves.'
+	},
+
+	['coke_leaf'] = {
+		label = 'Coca Leaf',
+		weight = 100,
+		stack = true,
+		description = 'Raw leaves from the coca plant, used for processing.'
+	},
+
+	['coke_bag'] = {
+		label = 'Cocaine Baggy',
+		weight = 15,
+		stack = true,
+		consume = 0,
+		description = 'A small plastic bag containing refined cocaine powder.'
+	},
+
+	['empty_bag'] = {
+		label = 'Empty Baggy',
+		weight = 5,
+		stack = true,
+		description = 'A small, sealable plastic bag used for packaging products.'
+	},
+
+	['coke_access'] = {
+		label = 'Laboratory Access Card',
+		weight = 15,
+		stack = true,
+		description = 'An encrypted keycard used to access drug labs.'
+	},
+
+	['coke'] = {
+		label = 'Cocaine',
+		weight = 5,
+		stack = true,
+		description = 'Refined white powder, almost ready for distribution or consumption.'
+	},
+
+	['trowel'] = {
+		label = 'Trowel',
+		weight = 100,
+		stack = false,
+		durability = 100,
+		description = 'A small handheld tool used for digging holes or harvesting plants.'
+	},
